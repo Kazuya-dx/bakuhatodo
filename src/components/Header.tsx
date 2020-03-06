79e3ad3754973883c6176b2dccba7c504aa4f7d2
三浦和也 <miurakazuya@Kazuya.local>
2020-03-06 16:30:02 +0900

    爆発エフェクト追加、PCのCSS追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.scss';
+import './Animation.scss';
+import { CSSTransition } from 'react-transition-group';
 
 const Header: React.FC = () => {
+    const [wrap, setWrap] = useState(false);
+    const [check, setCheck] = useState(false);
+    const [check2, setCheck2] = useState(false);
+
+    useEffect(() => {
+        setWrap(true);
+    }, [wrap])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCheck(true);
+        }, 400);
+    }, [check])
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCheck2(true);
+        }, 800);
+    }, [check2])
     return (
         <header>
-            <h1>サボったら爆発するTodoアプリ</h1>
+            <CSSTransition
+                in={wrap}
+                classNames='wrap'
+                timeout={800}
+                unmountOnExit
+            >
+                <div className="wrap"></div>
+            </CSSTransition>
+            <CSSTransition
+                in={check}
+                classNames='fade'
+                timeout={800}
+                unmountOnExit
+            >
+                <h1>爆発するTodoアプリ</h1>
+            </CSSTransition>
+            <CSSTransition
+                in={check2}
+                classNames='fade'
+                timeout={800}
+                unmountOnExit
+            >
+                <h2>あなたがサボった時、タスクが爆発します。</h2>
+            </CSSTransition>
         </header>
     )
 }
--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react'
+import Img from "../statics/explosion.gif";
 import { Task } from '../Types'
 import './TaskItem.scss'
+import './Animation.scss'
 
 // Redux関連のモジュール・ファイル
 import { useDispatch } from 'react-redux'
 import { doneTask, deleteTask } from '../modules/tasksModule'
+import { CSSTransition } from 'react-transition-group'
 
 type Props = {
     task: Task
@@ -41,8 +44,10 @@ function useCountDown(initialValue: number, task: Task): number {
 
     // 制限時間に達した時の処理
     if (count === -1) {
-        alert('さようなら、' + task.title);
-        dispatch(deleteTask(task));
+        setTimeout(() => {
+            dispatch(deleteTask(task));
+            alert(task.title + 'が爆発しました');
+        }, 1750)
     }
 
     return count;
@@ -51,24 +56,38 @@ function useCountDown(initialValue: number, task: Task): number {
 const TaskItem: React.FC<Props> = ({ task }) => {
     // Actionを使用するために dispatch を定義
     const dispatch = useDispatch()
-
     const count: number = useCountDown(10, task);
+    const [check, setCheck] = useState(false);
 
-    return (
-        <li className={task.done ? 'done' : ''}>
-            <label>
-                <input
-                    type="checkbox"
-                    // Actionを使用する時は dispatch(アクション) のようにすること
-                    onClick={() => dispatch(doneTask(task))}
-                    defaultChecked={task.done}
-                />
-                <span>{task.title}</span>
-                {!task.done ? <p>爆発まで 残り {count} 秒</p> : <p>タスク完了</p>}
-            </label>
-            <button onClick={() => dispatch(deleteTask(task))}>削除</button>
-        </li>
-    )
+    useEffect(() => {
+        setCheck(true);
+    }, [check]);
+
+    return count >= 0 ? (
+        <CSSTransition
+            in={check}
+            classNames="fade"
+            timeout={800}
+            unmountOnExit
+        >
+            <li className={task.done ? 'done' : ''}>
+                <label>
+                    <input
+                        type="checkbox"
+                        // Actionを使用する時は dispatch(アクション) のようにすること
+                        onClick={() => dispatch(doneTask(task))}
+                        defaultChecked={task.done}
+                    />
+                    <span>{task.title}</span>
+                    {!task.done ? <p>爆発まで 残り {count} 秒</p> : <p>タスク完了</p>}
+                </label>
+                <button onClick={() => dispatch(deleteTask(task))}>削除</button>
+            </li>
+        </CSSTransition>
+    ) :
+        <div className="wrap">
+            <img src={Img} alt="" className="explosion" />
+        </div>
 }
 
 export default TaskItem
